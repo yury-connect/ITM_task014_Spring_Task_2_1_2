0c5f91e6a38b04fe4decdb2e094dfbe46aa15c86
Yury_Home-PC_Win10_worked <yury.connect@gmail.com>
2024-10-17 09:30:56 +0300

    solution: save -gпросто сохраняюсь

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,10 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+
+    @Bean(name="cat")
+    public Cat getCat() {
+        Cat cat = new Cat("Vaska");
+        return cat;
+    }
 }
